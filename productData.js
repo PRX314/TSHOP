1e07cc1f62ddb4efe9c572f7316de6481d4cbfc3
PRX314 <153088885+PRX314@users.noreply.github.com>
2025-04-18 18:28:32 +0200

    productData.js

--- productData.js
+++ productData.js
@@ -1,33 +1,32 @@
-
 const products = [
   {
     name: "Fijo de’n amore",
-    description: "Collezione ufficiale Fijo de’n amore",
+    description: "Collezione unisex Fijo – t-shirt e felpe con cuore rosso",
     themeClass: "theme-fijo",
     variants: [
       {
-        type: "Uomo",
+        type: "Maglietta",
         color: "Bianca",
         image: "images/FijoUomoBianca.png",
         price: 20,
         sizes: ["S", "M", "L", "XL"]
       },
       {
-        type: "Uomo",
+        type: "Maglietta",
         color: "Nera",
         image: "images/FijoUomoNera.png",
         price: 20,
         sizes: ["S", "M", "L", "XL"]
       },
       {
-        type: "Donna",
+        type: "Maglietta Donna",
         color: "Bianca",
         image: "images/FijoDonnaBianca.png",
         price: 20,
         sizes: ["S", "M", "L"]
       },
       {
-        type: "Donna",
+        type: "Maglietta Donna",
         color: "Nera",
         image: "images/FijoDonnaNera.png",
         price: 20,
@@ -44,48 +43,37 @@ const products = [
   },
   {
     name: "G Power",
-    description: "Collezione potente e audace",
+    description: "Collezione G Power – esplosiva e ironica, in versione uomo e donna",
     themeClass: "theme-gpower",
     variants: [
       {
-        type: "Uomo",
-        color: "Bianca",
-        image: "images/GPowerUomoBianca.png",
-        price: 22,
-        sizes: ["S", "M", "L", "XL"]
-      },
-      {
-        type: "Uomo",
-        color: "Nera",
-        image: "images/GPowerUomoNera.png",
-        price: 22,
-        sizes: ["S", "M", "L", "XL"]
-      },
-      {
-        type: "Donna",
+        type: "Maglietta Donna",
         color: "Bianca",
         image: "images/GPowerDonnaBianca.png",
         price: 22,
         sizes: ["S", "M", "L"]
       },
       {
-        type: "Donna",
+        type: "Maglietta Donna",
         color: "Nera",
         image: "images/GPowerDonnaNera.png",
         price: 22,
         sizes: ["S", "M", "L"]
+      },
+      {
+        type: "Maglietta Uomo",
+        color: "Bianca",
+        image: "images/GPowerUomoBianca.png",
+        price: 22,
+        sizes: ["M", "L", "XL"]
+      },
+      {
+        type: "Maglietta Uomo",
+        color: "Nera",
+        image: "images/GPowerUomoNera.png",
+        price: 22,
+        sizes: ["M", "L", "XL"]
       }
     ]
   }
 ];
-
-window.onload = function () {
-  const select = document.getElementById("collection-select");
-  products.forEach((p, i) => {
-    let opt = document.createElement("option");
-    opt.value = i;
-    opt.text = p.name;
-    select.appendChild(opt);
-  });
-  select.dispatchEvent(new Event("change"));
-};
